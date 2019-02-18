1f0614d9a860962d0231aa147573a674be6ee0d9
Derek <hsiaoderek@berkeley.edu>
2019-02-18 00:40:52 -0800

    clickable tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { active_tab: 1 }
+    this.setActiveTab = this.setActiveTab.bind(this)
+  }
+
+  setActiveTab(new_active) {
+    this.setState({ active_tab: new_active })
+  }
+
   render() {
     return (
       <div className="container">
+        <h1 className="title">Rev Bayes Code Generator</h1>
         <div className="tabs">
           <ul>
-            <li className="is-active"><a>File</a></li>
-            <li><a>Tree Constraints</a></li>`
-            <li><a>Code</a></li>
+            <li className={this.state.active_tab === 1 && "is-active"}>
+              <a onClick={() => this.setActiveTab(1)}>File</a>
+            </li>
+            <li className={this.state.active_tab === 2 && "is-active"}>
+              <a onClick={() => this.setActiveTab(2)}>Tree Constraints</a>
+            </li>
+            <li className={this.state.active_tab === 3 && "is-active"}>
+              <a onClick={() => this.setActiveTab(3)}>Code</a></li>
           </ul>
         </div>
       </div>
