8eafcfafdc32ad75060747e58662177ede240fc7
Maxwell Lee <maxdaelee@berkeley.edu>
2019-04-07 22:28:03 -0700

    recommit

--- templates/static/webpack.config.js
+++ templates/static/webpack.config.js
@@ -2,26 +2,26 @@ const webpack = require('webpack');
 const resolve = require('path').resolve;
 const config = {
     devtool: 'eval-source-map',
- entry: __dirname + '/src/index.js',
- output:{
-      path: resolve('../public'),
-      filename: 'bundle.js',
-      publicPath: resolve('../public')
-},
- resolve: {
-  extensions: ['.js','.jsx','.css']
- },
- module: {
-    rules: [
-    {
-        test: /\.jsx?/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
+    entry: __dirname + '/src/index.js',
+    output:{
+        path: resolve('../public'),
+        filename: 'bundle.js',
+        publicPath: resolve('../public')
     },
-    {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?modules'
-    }]
-   }
+    resolve: {
+        extensions: ['.js','.jsx','.css']
+    },
+    module: {
+        rules: [
+        {
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader?modules'
+        }]
+    }
 };
 module.exports = config;
